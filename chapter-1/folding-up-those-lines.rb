bb654dfd3a67af73be85136682075f2b3984fd0e
Christian Fei <info@christian-fei.com>
2014-04-14 22:53:12 +0200

    added "folding-up-those-lines" to chapter 1

---
+++ chapter-1/folding-up-those-lines.rb
@@ -0,0 +1,9 @@
+# in ruby you can have multiple statements per line
+#  separated by a semicolon
+puts 'food'; puts 'bar'
+
+# or a simple/empty class
+class DocumentException < Exception; end
+
+# or a method to overwrite
+def method_to_be_overridden; end
